94d0a3689046c118767b11cbdfa5194f88f2bbae
grandeto <grandeto.com@gmail.com>
2019-09-13 12:05:36 +0300

    Add poll_option validation rule

--- app/models/poll_option.rb
+++ app/models/poll_option.rb
@@ -1,6 +1,8 @@
 class PollOption < ApplicationRecord
   validates :option, presence: true, format: { with: /[a-zA-Z0-9_]/ }, length: { in: 3..255 }
   validates :priority, presence: true, numericality: true
+  validates :votes, numericality: true
+
   belongs_to :poll
   default_scope { order(priority: :asc) }
 end
