ae45e85edad31be211636f08c46b598648d2479e
grandeto <grandeto.com@gmail.com>
2019-09-13 12:02:39 +0300

    Create Auth helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  REALM = Rails.application.credentials.dig(:ADMIN_SECRET)
+  USERS = {
+          Rails.application.credentials.dig(:ADMIN_USER) => Rails.application.credentials.dig(:ADMIN_PASS),
+          Rails.application.credentials.dig(:ADMIN_DAP) => Digest::MD5.hexdigest([Rails.application.credentials.dig(:ADMIN_DAP),REALM,Rails.application.credentials.dig(:ADMIN_PASS)].join(":"))
+  }
+  # HTTP Digest Auth
+  def authenticate_helper
+    authenticate_or_request_with_http_digest(REALM) do |username|
+      USERS[username]
+    end
+  end
 end
