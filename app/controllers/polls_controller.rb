8e3de33fc8da2e29a6caa6e4ba659b9bb4118b9f
grandeto <grandeto.com@gmail.com>
2019-09-13 12:07:57 +0300

    Give polls  public access, modify polls methods and views to handle only public needed data

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -1,39 +1,18 @@
 class PollsController < ApplicationController
-  before_action :set_poll, only: [:show, :edit, :update, :destroy]
+  before_action :set_poll, only: [:show, :update]
 
   # GET /polls
   # GET /polls.json
   def index
-    @polls = Poll.all
+    @polls = Poll.joins(:poll_options).group('polls.id')
   end
 
   # GET /polls/1
   # GET /polls/1.json
   def show
-  end
-
-  # GET /polls/new
-  def new
-    # @poll_options = PollOption.all
-    @poll = Poll.new
-    2.times { @poll.poll_options.build }
-  end
-
-  # GET /polls/1/edit
-  def edit
-  end
-
-  # POST /polls
-  # POST /polls.json
-  def create
-    @poll = Poll.new(poll_params)
-    respond_to do |format|
-      if @poll.save
-        format.html { redirect_to @poll, notice: 'Poll was successfully created.' }
-        format.json { render :show, status: :created, location: @poll }
-      else
-        format.html { render :new }
-        format.json { render json: @poll.errors, status: :unprocessable_entity }
+    if @poll.poll_options.count < 2
+      respond_to do |format|
+        format.html { redirect_to polls_url }
       end
     end
   end
@@ -41,36 +20,28 @@ class PollsController < ApplicationController
   # PATCH/PUT /polls/1
   # PATCH/PUT /polls/1.json
   def update
-    @poll.poll_options.destroy_all
-    respond_to do |format|
-      if @poll.update(poll_params)
-        format.html { redirect_to @poll, notice: 'Poll was successfully updated.' }
-        format.json { render :show, status: :ok, location: @poll }
-      else
-        format.html { render :edit }
-        format.json { render json: @poll.errors, status: :unprocessable_entity }
+    @option = @poll.poll_options.where(id: params[:votes]).take
+    if @option.present? && (@option.poll_id.eql? params[:id].to_i)
+      respond_to do |format|
+        if @option.increment!(:votes)
+          format.html { redirect_to @poll, notice: 'Poll was successfully updated.' }
+          format.json { render :show, status: :ok, location: @poll }
+        else
+          format.html { render :edit }
+          format.json { render json: @poll.errors, status: :unprocessable_entity }
+        end
       end
-    end
-  end
-
-  # DELETE /polls/1
-  # DELETE /polls/1.json
-  def destroy
-    @poll.destroy
-    respond_to do |format|
-      format.html { redirect_to polls_url, notice: 'Poll was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_poll
-      @poll = Poll.find(params[:id])
+      @poll = Poll.includes(:poll_options).where(id: params[:id]).take
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def poll_params
-      params.require(:poll).permit(:name, poll_options_attributes: [:option, :priority])
+      params.require(:poll).permit(poll_options_attributes: [:id, :votes])
     end
 end
--- app/views/polls/show.json.jbuilder
+++ app/views/polls/show.json.jbuilder
@@ -1,1 +1,2 @@
 json.partial! "polls/poll", poll: @poll
+json.poll_options @poll.poll_options, partial: 'poll_options/poll_option', as: :poll_option
